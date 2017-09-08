d546e8a1215c838417f37894d326571df4a62ecd
Kamil Ciaston <Kamilciaston6@gmail.com>
2017-09-08 20:48:16 +0200

    created handler-after upload image it is send back to show what was uploaded

--- modules/handlers.js
+++ modules/handlers.js
@@ -23,6 +23,19 @@ exports.welcome = function(request, response) {
     });
 }
 
+
+exports.show = function(request, response) {
+    fs.readFile("test.png", "binary", function(error, file) {
+        response.writeHead(200, {"Content-Type": "image/png"});
+        response.write(file, "binary");
+        response.end();
+    }); 
+}
+
+
+
+
+
 exports.error = function(request, response) {
     console.log("houston we have problem");
     response.write("404 :(");
