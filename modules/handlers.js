7d31f30d288ae1fdf3b08eae8745d2da476bfbbd
Kamil Ciaston <Kamilciaston6@gmail.com>
2017-09-08 20:32:32 +0200

    added file start html with basic form and changed handler welcome

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,3 +1,6 @@
+var fs = require('fs');
+
+
 exports.upload = function(request, response) {
     console.log("starting upload");
     response.write("upload has started");
@@ -5,13 +8,16 @@ exports.upload = function(request, response) {
 }
 
 exports.welcome = function(request, response) {
-    console.log("starting welcome.");
-    response.write("Welcome!");
-    response.end();
+    console.log("request has started.");
+    fs.readFile('templates/start.html', function(err, html) {
+        response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+        response.write(html);
+        response.end();
+    });
 }
 
 exports.error = function(request, response) {
-    console.log("houston we have problem");
+    console.log("houston we have problem"); 
     response.write("404 :(");
     response.end();
 }
